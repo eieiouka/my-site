1fb68630dc4b87d3b83ab8594c9ff28501ab5169
eieiouka <eieiyukkuri@gmail.com>
2026-04-12 20:56:30 +0900

    Aboutに吹き出し追加

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -1,36 +1,57 @@
+import { useState } from "react";
 import "./Gallery.css";
 
+const images = [
+  { src: "/images/sample1.jpg", alt: "ナノちゃんのイラスト1" },
+  { src: "/images/sample2.jpg", alt: "ナノちゃんのイラスト2" },
+  { src: "/images/sample3.jpg", alt: "ナノちゃんのイラスト3" },
+  { src: "/images/sample4.jpg", alt: "ナノちゃんのイラスト4" },
+  { src: "/images/sample5.jpg", alt: "ナノちゃんのイラスト5" },
+  { src: "/images/sample6.jpg", alt: "ナノちゃんのイラスト6" },
+];
+
 export default function Gallery() {
+  const [selectedImage, setSelectedImage] = useState(null);
+
   return (
     <section id="gallery" className="section gallery">
       <p className="section-label">Gallery</p>
       <h2>私の可愛い写真集</h2>
 
       <div className="gallery-grid">
-        <div className="gallery-item">
-          <img src="/images/sample1.jpg" alt="ナノちゃんのイラスト1" />
-        </div>
-
-        <div className="gallery-item">
-          <img src="/images/sample2.jpg" alt="ナノちゃんのイラスト2" />
-        </div>
-
-        <div className="gallery-item">
-          <img src="/images/sample3.jpg" alt="ナノちゃんのイラスト3" />
-        </div>
-
-        <div className="gallery-item">
-          <img src="/images/sample4.jpg" alt="ナノちゃんのイラスト4" />
-        </div>
-
-        <div className="gallery-item">
-          <img src="/images/sample5.jpg" alt="ナノちゃんのイラスト5" />
-        </div>
-
-        <div className="gallery-item">
-          <img src="/images/sample6.jpg" alt="ナノちゃんのイラスト6" />
-        </div>
+        {images.map((img) => (
+          <button
+            key={img.src}
+            className="gallery-item"
+            onClick={() => setSelectedImage(img)}
+          >
+            <img src={img.src} alt={img.alt} />
+          </button>
+        ))}
       </div>
+
+      {/* モーダル */}
+      {selectedImage && (
+        <div className="gallery-modal" onClick={() => setSelectedImage(null)}>
+          <div
+            className="gallery-modal-content"
+            onClick={(e) => e.stopPropagation()}
+          >
+            <button
+              className="gallery-close"
+              onClick={() => setSelectedImage(null)}
+            >
+              ×
+            </button>
+
+            <img
+              className="gallery-modal-image"
+              src={selectedImage.src}
+              alt={selectedImage.alt}
+            />
+          </div>
+        </div>
+      )}
     </section>
   );
 }
